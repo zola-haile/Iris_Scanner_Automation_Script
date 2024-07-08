36f62fe3b0722e75dfbdddf1a6de771de30ca9a0
Czajka Demo1 <czajkademo1@dhcp-10-7-29-149.dhcp.nd.edu>
2024-07-08 15:47:20 -0400

    Last commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pexpect
 import csv
 import os
+import re
 
 
 from skimage.metrics import structural_similarity as ssim
@@ -9,32 +10,29 @@ import imutils
 import cv2
 
 
-from nameReader import nameList 
-
-from nameReader2 import nameList2
 
 
+firstTime=input("Is this your first run(y/n): ")
+
+if firstTime.lower()=='y':
+    #If this is the first run, uncomment the following two lines
+    from nameReader import nameList,nameList2
+
+
+elif firstTime.lower()=='n':
+    #If not first run, uncomment the following:
+    os.system("python3 /Users/czajkademo1/Desktop/pyDemo/name_to_be_scanned.py")
+    from name_to_be_scanned import nameList,nameList2
+
+
+
+#image name used for naming
 imgName=nameList
 
-nameList=nameList2
+# #image name used for checking similarity
 
+# nameList=nameList2
 
-
-
-
-
-
-# *****************#########$$$$$$$$$%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-# *****************#########$$$$$$$$$%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-#               Extract the name of the image from csv file and put it in a list
-# *****************#########$$$$$$$$$%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-# ******************#########$$$$$$$$$%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-
-
-# *****************#########$$$$$$$$$%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-# *****************#########$$$$$$$$$%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-# *****************#########$$$$$$$$$%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-# ******************#########$$$$$$$$$%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
 
 def expect_and_send(sendValue):
@@ -72,21 +70,21 @@ while loop:
     child.sendline("")
 
     # Reset on open device: 
-    # 	1. Yes (default)
-    # 	2. No
+    #   1. Yes (default)
+    #   2. No
 
     expect_and_send("1")
 
     # MAIN MENU: Select one of the features below
-    # 	1. Login
-    # 	2. Logout
-    # 	3. Device Management
-    # 	4. Device & SDK Information
-    # 	5. Capturing Process
-    # 	6. Iris Recognition
-    # 	7. Power Management
-    # 	8. Recovery (IriShield USB only)
-    # 	9. Exit
+    #   1. Login
+    #   2. Logout
+    #   3. Device Management
+    #   4. Device & SDK Information
+    #   5. Capturing Process
+    #   6. Iris Recognition
+    #   7. Power Management
+    #   8. Recovery (IriShield USB only)
+    #   9. Exit
     # Enter your choice:
 
 
@@ -95,8 +93,8 @@ while loop:
 
     # Parameters for capturing process
     # Capture mode: 
-    # 	1. IDDK_TIMEBASED (default) 
-    # 	2. IDDK_FRAMEBASED
+    #   1. IDDK_TIMEBASED (default) 
+    #   2. IDDK_FRAMEBASED
     # Enter your choice: 
 
 
@@ -109,16 +107,16 @@ while loop:
     child.sendline("5")
 
     # Quality mode: 
-    # 	1. Normal (default)
-    # 	2. High 
-    # 	3. Very High
+    #   1. Normal (default)
+    #   2. High 
+    #   3. Very High
     # Enter your choice:
 
     expect_and_send("3")
 
     # Enable auto led? 
-    # 	1. Yes (default)
-    # 	2. No
+    #   1. Yes (default)
+    #   2. No
     # Enter your choice:
 
 
@@ -141,23 +139,23 @@ while loop:
         if index==0 or index==1:
 
             # MAIN MENU: Select one of the features below
-            # 	1. Login
-            # 	2. Logout
-            # 	3. Device Management
-            # 	4. Device & SDK Information
-            # 	5. Capturing Process
-            # 	6. Iris Recognition
-            # 	7. Power Management
-            # 	8. Recovery (IriShield USB only)
-            # 	9. Exit
+            #   1. Login
+            #   2. Logout
+            #   3. Device Management
+            #   4. Device & SDK Information
+            #   5. Capturing Process
+            #   6. Iris Recognition
+            #   7. Power Management
+            #   8. Recovery (IriShield USB only)
+            #   9. Exit
             # Enter your choice:
 
             expect_and_send("5")
 
             # Parameters for capturing process
             # Capture mode: 
-            # 	1. IDDK_TIMEBASED (default) 
-            # 	2. IDDK_FRAMEBASED
+            #   1. IDDK_TIMEBASED (default) 
+            #   2. IDDK_FRAMEBASED
             # Enter your choice: 
 
             expect_and_send("1")
@@ -168,16 +166,16 @@ while loop:
             child.sendline("5")
 
             # Quality mode: 
-            # 	1. Normal (default)
-            # 	2. High 
-            # 	3. Very High
+            #   1. Normal (default)
+            #   2. High 
+            #   3. Very High
             # Enter your choice:
 
             expect_and_send("2")
 
             # Enable auto led? 
-            # 	1. Yes (default)
-            # 	2. No
+            #   1. Yes (default)
+            #   2. No
             # Enter your choice:
 
 
@@ -193,20 +191,28 @@ while loop:
             count+=1
             if count==5:
                 # MAIN MENU: Select one of the features below
-            # 	1. Login
-            # 	2. Logout
-            # 	3. Device Management
-            # 	4. Device & SDK Information
-            # 	5. Capturing Process
-            # 	6. Iris Recognition
-            # 	7. Power Management
-            # 	8. Recovery (IriShield USB only)
-            # 	9. Exit
+            #   1. Login
+            #   2. Logout
+            #   3. Device Management
+            #   4. Device & SDK Information
+            #   5. Capturing Process
+            #   6. Iris Recognition
+            #   7. Power Management
+            #   8. Recovery (IriShield USB only)
+            #   9. Exit
             # Enter your choice:
 
                 expect_and_send("9")
 
                 list_of_unscanned_iris.append(imgName[0])
+
+
+                with open("unscanned_imgName.csv",'a', newline='') as csvfile:
+                    csvwriter=csv.writer(csvfile)
+
+                    csvwriter.writerows(imgName[0])
+
+
                 print("****  This iris could not be scanned, so it is skiped ***")
                 break
 
@@ -219,26 +225,26 @@ while loop:
 
 
     # Do you want to get the result image? 
-    # 	1. No (default)
-    # 	2. Yes
+    #   1. No (default)
+    #   2. Yes
     # Enter your choice: 2
     if index==2:
         expect_and_send("2")
 
         # Select image kind: 
-        # 	1. Original Image - K1 (default) 
-        # 	2. VGA Image - K2 
-        # 	3. Cropped Image - K3
-        # 	4. Cropped and Masked Image - K7
+        #   1. Original Image - K1 (default) 
+        #   2. VGA Image - K2 
+        #   3. Cropped Image - K3
+        #   4. Cropped and Masked Image - K7
         # Enter your choice:
 
         expect_and_send("1")
 
         # Select image format: 
-        # 	1. Mono JP2 Image (default)
-        # 	2. Mono Raw Image
-        # 	3. IriTech JP2 Image
-        # 	4. IriTech Raw Image
+        #   1. Mono JP2 Image (default)
+        #   2. Mono Raw Image
+        #   3. IriTech JP2 Image
+        #   4. IriTech Raw Image
         # Enter your choice: 1
 
         expect_and_send("1")
@@ -249,15 +255,73 @@ while loop:
         child.sendline("50")
 
         # Do you want to get result ISO image: 
-        # 	1. No (default)
-        # 	2. Yes
+        #   1. No (default)
+        #   2. Yes
         # Enter your choice:
 
         expect_and_send("1")
 
 
+        # #Try another capture?
+        #     # 	1. Yes (default)
+        #     # 	2. No
+        #     # Enter your choice: 2
+
+        # expect_and_send("2")
+
+        # #   MAIN MENU: Select one of the features below
+        # # 	1. Login
+        # # 	2. Logout
+        # # 	3. Device Management
+        # # 	4. Device & SDK Information
+        # # 	5. Capturing Process
+        # # 	6. Iris Recognition
+        # # 	7. Power Management
+        # # 	8. Recovery (IriShield USB only)
+        # # 	9. Exit
+        # # Enter your choice: 6
+
+        # expect_and_send("6")
+
+        # #         IRIS RECOGNITION: Please select one menu item 
+        # # 	1. Main Menu
+        # # 	2. Get Gallery Information
+        # # 	3. Enroll Captured Iris
+        # # 	4. Get Result Template
+        # # 	5. Enroll Template
+        # # 	6. Unenroll Templates
+        # # 	7. Matching
+        # # 	8. Exit
+        # # Enter your choice: 7
+
+        # expect_and_send("7")
+
+
+        # #         Select the kind of comparison: 
+        # # 	1. Compare11 (default)
+        # # 	2. Compare1N
+        # # 	3. Compare11WithTemplate
+        # # Enter your choice: 1
+
+        # expect_and_send("1")
+
+        # # Enter the enrollee ID (less than 32 characters): 
+
+        # child.expect("Enter the enrollee ID (less than 32 characters): ")
+        # child.sendline(imgName[0])
+
+        # # Regular expression to find the distance value
+        # match = re.search(r'Compare Distance = (\d+\.\d+)', output)
+        # if match:
+        #     distance_wanted=float(match.group(1))
+        #     print(distance_wanted)
+        
         # Close the log file
+        
+        
         child.logfile.close()
+    
+
 
 
 
@@ -266,32 +330,31 @@ while loop:
     # *****************#########$$$$$$$$$%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
 
-        captured_image_path = "/opt/IriTech/IDDK-2000-3.3.1-OSX/demo/source/UnknownEyeImage_1.jp2"
-        original_image = f"/Users/czajkademo1/Desktop/pyDemo/check_similarity/CrossImages/{nameList[0]}"
+        # captured_image_path = "/opt/IriTech/IDDK-2000-3.3.1-OSX/demo/source/UnknownEyeImage_1.jp2"
+        # original_image = f"/Users/czajkademo1/Desktop/pyDemo/check_similarity/CrossImages/{nameList[0]}"
 
-        imageA = cv2.imread(captured_image_path)	
+        # imageA = cv2.imread(captured_image_path)    
         
-        imageB = cv2.imread(original_image)
+        # imageB = cv2.imread(original_image)
         
-        # convert the images to grayscale
-        grayA = cv2.cvtColor(imageA, cv2.COLOR_BGR2GRAY)
-        grayB = cv2.cvtColor(imageB, cv2.COLOR_BGR2GRAY)# compute the Structural Similarity Index (SSIM) between the two
-        # images, ensuring that the difference image is returned
-        (score, diff) = ssim(grayA, grayB, full=True)
-        print(score)
-        diff = (diff * 255).astype("uint8")
+        # # convert the images to grayscale
+        # grayA = cv2.cvtColor(imageA, cv2.COLOR_BGR2GRAY)
+        # grayB = cv2.cvtColor(imageB, cv2.COLOR_BGR2GRAY)# compute the Structural Similarity Index (SSIM) between the two
+        # # images, ensuring that the difference image is returned
+        # (score, diff) = ssim(grayA, grayB, full=True)
+        # print(score)
+        # diff = (diff * 255).astype("uint8")
         
 
-        if score<0.9:
-            child=pexpect.spawn(f"mv /opt/IriTech/IDDK-2000-3.3.1-OSX/demo/source/UnknownEyeImage_1.jp2 /Users/czajkademo1/Desktop/pyDemo/saved_images/{imgName[0]}.jp2")
-            print("Process completed.")
+        # if score<0.9:
 
-
+        child=pexpect.spawn(f"mv /opt/IriTech/IDDK-2000-3.3.1-OSX/demo/source/UnknownEyeImage_1.jp2 /Users/czajkademo1/Desktop/pyDemo/saved_images/{imgName[0]}.jp2")
+        print("Process completed.")
 
 
 
     imgName.pop(0)
-    nameList.pop(0)
+    # nameList.pop(0)
     
     
     number_imgs_taken+=1
@@ -304,9 +367,11 @@ while loop:
 
 
 
+
+
 fileName_imgName_comparison="imgName_comparison.csv"
 fileName_imgName_naming="imgName_naming.csv"
-fileName_unscanned_imgName="unscanned_imgName.csv"
+
 
 with open(fileName_imgName_comparison,'w', newline='') as csvfile:
     csvwriter=csv.writer(csvfile)
@@ -321,9 +386,5 @@ with open(fileName_imgName_naming,'w', newline='') as csvfile:
     for item in imgName:
         csvwriter.writerow([item])
 
-with open(fileName_unscanned_imgName,'w', newline='') as csvfile:
-    csvwriter=csv.writer(csvfile)
 
-    for item in list_of_unscanned_iris:
-        csvwriter.writerow([item])
 
